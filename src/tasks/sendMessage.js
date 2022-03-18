da5e1f3a4d600e1a24338ac10b210a837769c5c8
Paulo Abreu <micaeler00@gmail.com>
2022-03-17 21:43:19 -0300

    feat: add new feat

--- src/tasks/sendMessage.js
+++ src/tasks/sendMessage.js
@@ -1,7 +1,6 @@
 const cron = require("node-cron");
 const { sendMessageRemember } = require("../services/twilio.js");
 
-const every5seconds = "*/5 * * * * *";
 const everyDayAt12Am = "0 0 12 1/1 *";
 const everyDayAt17Pm = "0 0 17 1/1 *";
 
@@ -18,7 +17,7 @@ const sendMessageAt12Am = cron.schedule(
 const sendMessageAt17Pm = cron.schedule(
   everyDayAt17Pm,
   () => {
-    sendMessageRemembe();
+    sendMessageRemember();
   },
   {
     scheduled: false,
